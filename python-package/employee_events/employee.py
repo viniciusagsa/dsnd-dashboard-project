78eb6ee2e114d208f1b46c923a01e7d96862e7d7
Vinicius Sá <viniciusagsa@gmail.com>
2025-09-28 16:44:12 -0300

    Implement SQL query methods in Employee and QueryBase classes

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,24 +1,24 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from employee_events.query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+from sql_execution import query, QueryMixin
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
+    name = 'employee'
 
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 3
         # Write an SQL query
@@ -27,14 +27,20 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
+        query = f"""
+            SELECT
+                CONCAT(first_name, ' ', last_name) AS full_name,
+                employee_id
+            FROM {self.name};
+        """
+        return query(query).fetchall()
     
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -42,7 +48,13 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
+        query = f"""
+            SELECT
+                CONCAT(first_name, ' ', last_name) AS full_name
+            FROM {self.name}
+            WHERE employee_id = {id};
+        """
+        return query(query).fetchall()
 
 
     # Below is method with an SQL query
@@ -52,10 +64,9 @@
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
+    
     def model_data(self, id):
-
-        return f"""
+        query = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +74,4 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        return self.pandas_query(query)
--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,37 +1,43 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from employee_events.query_base import QueryBase
 
 # Import dependencies for sql execution
-#### YOUR CODE HERE
+from sql_execution import query, QueryMixin
 
 # Create a subclass of QueryBase
 # called  `Team`
-#### YOUR CODE HERE
+class Team(QueryBase, QueryMixin):
 
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
+    name = 'team'
 
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
-        #### YOUR CODE HERE
+        query = f"""
+            SELECT
+                team_name,
+                team_id
+            FROM {self.name};
+        """
+        return self.query(query).fetchall()
     
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    def team_name(self, id):
 
         # Query 6
         # Write an SQL query
@@ -39,7 +45,13 @@
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-        #### YOUR CODE HERE
+        query = f"""
+            SELECT
+                team_name
+            FROM {self.name}
+            WHERE team_id = {id};
+        """
+        return self.query(query).fetchall()
 
 
     # Below is method with an SQL query
@@ -52,7 +64,7 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        query = f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
@@ -64,3 +76,4 @@
                     GROUP BY employee_id
                    )
                 """
+        return self.pandas_query(query)
